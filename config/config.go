e0515a46586447c2c95de62e28833392b6f8cd35
ifpj <57943069+ifpj@users.noreply.github.com>
2026-04-07 08:30:17 +0800

    添加udpme协议支持

--- config/config.go
+++ config/config.go
@@ -1196,6 +1196,9 @@ func parseNameServer(servers []string, respectRules bool, preferH3 bool) ([]dns.
 		case "udp":
 			addr, err = hostWithDefaultPort(u.Host, "53")
 			dnsNetType = "" // UDP
+		case "udpme":
+			addr, err = hostWithDefaultPort(u.Host, "53")
+			dnsNetType = "udpme"
 		case "tcp":
 			addr, err = hostWithDefaultPort(u.Host, "53")
 			dnsNetType = "tcp" // TCP
