3a02c3747f19876ec681eeb89abaf5625a391bfa
ifpj <57943069+ifpj@users.noreply.github.com>
2026-04-07 08:30:17 +0800

    添加turn出口支持

--- constant/adapters.go
+++ constant/adapters.go
@@ -48,6 +48,7 @@ const (
 	AnyTLS
 	Sudoku
 	Masque
+	Turn
 	TrustTunnel
 	ShadowQuic
 	OpenVPN
@@ -227,6 +228,8 @@ func (at AdapterType) String() string {
 		return "Sudoku"
 	case Masque:
 		return "Masque"
+	case Turn:
+		return "Turn"
 	case TrustTunnel:
 		return "TrustTunnel"
 	case ShadowQuic:
